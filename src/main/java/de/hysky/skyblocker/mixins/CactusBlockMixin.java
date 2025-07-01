33da2508cfd937e8c7a868da8716c0eea995845e
Luna <lunaynx@gmail.com>
2025-07-01 02:12:22 +0200

    Fix

--- src/main/java/de/hysky/skyblocker/mixins/CactusBlockMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/CactusBlockMixin.java
@@ -5,6 +5,7 @@ import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.utils.Utils;
 import net.minecraft.block.*;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -12,23 +13,12 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(CactusBlock.class)
 public abstract class CactusBlockMixin extends Block {
-	@Shadow
-	protected static VoxelShape SHAPE;
-
-	@Unique
-	private static final VoxelShape OLD_SHAPE = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 16.0, 16.0);
-
 	public CactusBlockMixin(Settings settings) {
 		super(settings);
 	}
 
 	@ModifyReturnValue(method = "getOutlineShape", at = @At("RETURN"))
 	private VoxelShape skyblocker$getCactusOutline(VoxelShape original) {
-		return Utils.isOnSkyblock() && SkyblockerConfigManager.get().general.hitbox.oldCactusHitbox ? OLD_SHAPE : original;
-	}
-
-	@Override
-	public VoxelShape getCullingShape(BlockState state) {
-		return SHAPE;
+		return Utils.isOnSkyblock() && SkyblockerConfigManager.get().general.hitbox.oldCactusHitbox ? VoxelShapes.fullCube() : original;
 	}
 }
